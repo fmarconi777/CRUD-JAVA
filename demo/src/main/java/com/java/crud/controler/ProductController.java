7f17b078621afae17ab927aedd399445ec3c0e35
Felipe Pedrosa <marconizero@gmail.com>
2024-04-18 12:52:05 -0300

    feat: add put method updateProduct in ProductController

--- demo/src/main/java/com/java/crud/controler/ProductController.java
+++ demo/src/main/java/com/java/crud/controler/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.java.crud.domain.product.Product;
 import com.java.crud.domain.product.ProductRepository;
 import com.java.crud.domain.product.RequestProduct;
 
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 @RestController
@@ -34,6 +36,14 @@ public class ProductController {
     public ResponseEntity<String> registerProduct(@RequestBody @Valid RequestProduct data) {
         Product newProduct = new Product(data);
         repository.save(newProduct);
+        return ResponseEntity.status(201).build();
+    }
+    @PutMapping
+    @Transactional
+    public ResponseEntity<String> updateProduct(@RequestBody @Valid RequestProduct data) {
+        Product product = repository.getReferenceById(data.id());
+        product.setName(data.name());
+        product.setPrice_in_cents(data.price_in_cents());
         return ResponseEntity.status(201).build();
     }
 }
