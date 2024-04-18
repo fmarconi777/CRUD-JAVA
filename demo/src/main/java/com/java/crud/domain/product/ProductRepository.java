6ffb99fdbaa5fa6c728309344cdb68ee176ac0e1
Felipe Pedrosa <marconizero@gmail.com>
2024-04-18 13:12:36 -0300

    feat: add findAllByActiveTrue method on ProductRepository

--- demo/src/main/java/com/java/crud/domain/product/ProductRepository.java
+++ demo/src/main/java/com/java/crud/domain/product/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.java.crud.domain.product;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, String> {
-    
+    List<Product> findAllByActiveTrue();
 }
