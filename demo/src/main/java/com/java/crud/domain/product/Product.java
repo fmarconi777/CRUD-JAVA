19ee5f12a4e8633bce29c9566219c83cb679964f
Felipe Pedrosa <marconizero@gmail.com>
2024-04-18 12:13:27 -0300

    feat: add Product entity on domain

---
+++ demo/src/main/java/com/java/crud/domain/product/Product.java
@@ -0,0 +1,30 @@
+package com.java.crud.domain.product;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "product")
+@Entity(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Double price_in_cents;
+
+    private Boolean active;
+}
